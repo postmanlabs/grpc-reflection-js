844ba80e18ba2225f360d32c9fdc15a5efbf999d
Appurva Murawat <appurva21@proton.me>
2025-10-01 17:53:26 +0530

    fix: remove useMapField option usage

--- src/descriptor.js
+++ src/descriptor.js
@@ -11,30 +11,25 @@ const set = require('lodash.set');
  * Get Protobuf.js Root object from the serialized FileDescriptorProto messages
  * that gotten from reflection service.
  * @param {Array<Uint8Array|string>|undefined} file_descriptor_protos - Reflection descriptor protos
- * @param {boolean} [useMapField=false] - Use experimental map field decoding support of protobufjs
  * @return {Root} Protobuf.js Root object
  */
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
-export function getDescriptorRoot(file_descriptor_protos, useMapField = false) {
+export function getDescriptorRoot(file_descriptor_protos) {
   const descriptorSet = Descriptor.FileDescriptorSet.create();
 
   file_descriptor_protos.forEach((descriptorByte, i) => {
     const descriptor = Descriptor.FileDescriptorProto.decode(descriptorByte);
     set(descriptorSet, 'file[' + i + ']', descriptor);
   });
-  return protobuf.Root.fromDescriptor(descriptorSet, {useMapField});
+  return protobuf.Root.fromDescriptor(descriptorSet);
 }
 
 /**
  * Get Protobuf.js Root object from FileDescriptorSet
  * @param {Message file_descriptor_set - File descriptor set
- * @param {boolean} [useMapField=false] - Use experimental map field decoding support of protobufjs
  * @return {Root} Protobuf.js Root object
  */
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
-export function getDescriptorRootFromDescriptorSet(
-  file_descriptor_set,
-  useMapField = false
-) {
-  return protobuf.Root.fromDescriptor(file_descriptor_set, {useMapField});
+export function getDescriptorRootFromDescriptorSet(file_descriptor_set) {
+  return protobuf.Root.fromDescriptor(file_descriptor_set);
 }
